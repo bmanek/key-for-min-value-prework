731487a50a6273384ee00c28d295b084979b0f1c
Ben Manekin <ben.manekin@gmail.com>
2019-02-07 19:29:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash == {}
+    puts "whoops"
+  else
     smallest_key = 0
     smallest_value = 100000
     name_hash.each do |key, value|
@@ -11,6 +14,7 @@ def key_for_min_value(name_hash)
       end
     end
     smallest_key
+  end
 end
 
 
