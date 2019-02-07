ecea8f79aa3e0c6c91732b0774526a8a55724e71
Ben Manekin <ben.manekin@gmail.com>
2019-02-07 19:27:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     puts "whoops"
   else
-    smallest_key = nil
-    smallest_value = nil
+    smallest_key = 0
+    smallest_value = 100000
     name_hash.each do |key, value|
       if value < smallest_value
         smallest_value = value
